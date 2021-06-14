f356f0c6a2ed2ae81ef7e6b29c5fcda1efa61eaf
bigovitch <85135769+bigovitch@users.noreply.github.com>
2021-06-14 15:02:11 -0600

    projectno.2

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    
+          $(document).ready(function(){
+            $('.carousel').carousel({
+              interval: 2000
+            })
+          }); 
+          $(document).ready(function(){
+          $('content').css("background-color" , "lightblue");
+          })
+$(".card-title").css("background-color" , "blue")   ;
